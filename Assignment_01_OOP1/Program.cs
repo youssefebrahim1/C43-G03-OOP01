72b28318b2b8798c6b3ef40f116f1792472630cc
Youssef Ibrahim <youssefebrahimhosni@gmail.com>
2025-01-10 13:05:41 +0200

    add common library

--- Assignment_01_OOP1/Program.cs
+++ Assignment_01_OOP1/Program.cs
@@ -1,126 +1,18 @@
 ﻿using System;
-//Q1 Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
-enum WeekDays
-{
-    Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
-}
-
-
- //Q2.	Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
-/*
-struct Person
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-}
-*/
-
-
-
-//Q3. Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
-
-enum Season
-{
-    Spring,Summer,Autumn,Winter
-}
-
-
-//Q5.	Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
-enum Colors
-{
-    Red,
-    Green,
-    Blue
-}
-
-//Q6.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
-struct Point
-{   
-    public double X { get; set; }
-    public double Y { get; set; }
-
-    public Point(double x, double y)
-    {
-        X = x;
-        Y = y;
-    }
-}
-
-//Q7.Create a struct called "Person" with properties "Name" and "Age". Write a C# prog.
-struct Person
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-}
-//Last Question (8)
-struct Rectangle
-{
-    private double width;
-    private double height;
-
-    public double Width
-    {
-        get { return width; }
-        set
-        {
-            if (value < 0)
-            {
-                Console.WriteLine("Width cannot be negative.");
-            }
-            else
-            {
-                width = value;
-            }
-        }
-    }
-
-    public double Height
-    {
-        get { return height; }
-        set
-        {
-            if (value < 0)
-            {
-                Console.WriteLine("Height cannot be negative.");
-            }
-            else
-            {
-                height = value;
-            }
-        }
-    }
-
-    public double Area
-    {
-        get { return width * height; }
-    }
-
-    public void Info()
-    {
-        Console.WriteLine($"Width: {Width}, Height: {Height}, Area: {Area}");
-    }
-}
-
-
-  
-  
-  
-    
-
-
+using Common;
 class Program
 {
     static void Main(string[] args)
     {
-        /*
+        
         #region Q1.
         foreach (var day in Enum.GetValues(typeof(WeekDays)))
         {
             Console.WriteLine(day);
         }
         #endregion
-        */
-        /*
+        
+        
         #region Q2.
             Person[] persons = new Person[3];
             persons[0] = new Person { Name = "Youssef", Age = 24 };
@@ -132,8 +24,8 @@ class Program
                 Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
             }
     #endregion
-        */
-        /*
+        
+        
         #region Q3.
         Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter): ");
         string input = Console.ReadLine();
@@ -164,11 +56,11 @@ class Program
         }
     
     #endregion
-        */
-        /*
+        
+        
         #region Q5.
         Console.WriteLine("Enter a color (Red, Green, Blue): ");
-        string input = Console.ReadLine();
+        string inputt = Console.ReadLine();
 
         if (Enum.TryParse(input, true, out Colors color))
         {
@@ -176,12 +68,12 @@ class Program
         }
         else
         {
-            Console.WriteLine($"{input} is not a primary color.");
+            Console.WriteLine($"{inputt} is not a primary color.");
         }
     
     #endregion
-        */
-        /*
+        
+        
         #region Q6. 
         try
         {
@@ -211,12 +103,12 @@ class Program
             Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
         #endregion
-        */
-        /*
+        
+        
         #region Q7. 
-        Person[] persons = new Person[3];
+        Person[] personss = new Person[3];
 
-        for (int i = 0; i < persons.Length; i++)
+        for (int i = 0; i < personss.Length; i++)
         {
             Console.WriteLine($"Enter details for person {i + 1}:");
             Console.Write("Name: ");
@@ -227,8 +119,8 @@ class Program
             persons[i] = new Person { Name = name, Age = age };
         }
 
-        Person oldest = persons[0];
-        foreach (var person in persons)
+        Person oldest = personss[0];
+        foreach (var person in personss)
         {
             if (person.Age > oldest.Age)
             {
@@ -237,8 +129,8 @@ class Program
         }
             Console.WriteLine($"The oldest person is {oldest.Name}, aged {oldest.Age}.");
             #endregion
-        */
-        /*
+        
+        
         #region LastQuestion
         Rectangle rect = new Rectangle();
 
@@ -251,7 +143,7 @@ class Program
         rect.Info();
 
         #endregion
-        */
+        
 
 
 
---
+++ Common/Common.cs
@@ -0,0 +1,100 @@
+﻿namespace Common { 
+            //Q1 Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+       public enum WeekDays
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+    //Q2.	Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+    //Q7.Create a struct called "Person" with properties "Name" and "Age". Write a C# prog.
+   public struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    //Q3. Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+    public enum Season
+    {
+        Spring, Summer, Autumn, Winter
+    }
+
+
+    //Q5.	Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+  public  enum Colors
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+
+    //Q6.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+   public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+
+
+
+
+    //Last Question (8)
+   public struct Rectangle
+    {
+        private double width;
+        private double height;
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Width cannot be negative.");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Height cannot be negative.");
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public double Area
+        {
+            get { return width * height; }
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Width: {Width}, Height: {Height}, Area: {Area}");
+        }
+    }
+
+}
+
